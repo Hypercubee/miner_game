3ff6b0dffab03717a290dad03fcebb7076116f51
Hypercube <hypercube.hy@gmail.com>
2025-02-08 15:00:03 +0100

    moved int type definitions into int_types.h

--- src/main.c
+++ src/main.c
@@ -4,15 +4,7 @@
 //#include <raylib.h>
 #include "../lib/raylib.h" // for autocomplete to recognise the .h
 
-// define ints
-typedef unsigned char u8;
-typedef char i8;
-typedef unsigned short u16;
-typedef short i16;
-typedef unsigned int u32;
-typedef int i32;
-typedef unsigned long u64;
-typedef long i64;
+#include "int_types.h"
 
 #include "ores.h"
 #include "world.h"
