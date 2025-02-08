3790627d3d858630939a497d3974dd70fcadc04e
Stanislav Chlup <standachlupcz@gmail.com>
2025-02-08 15:00:03 +0100

    moved int type definitions into int_types.h

---
+++ src/int_typed.h
@@ -0,0 +1,13 @@
+#ifndef INT_TYPES_H
+#define INT_TYPES_H
+
+typedef unsigned char u8;
+typedef char i8;
+typedef unsigned short u16;
+typedef short i16;
+typedef unsigned int u32;
+typedef int i32;
+typedef unsigned long u64;
+typedef long i64;
+
+#endif // INT_TYPES_H
